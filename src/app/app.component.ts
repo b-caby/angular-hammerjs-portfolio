895b1644a0c61dd071dc7b174f5ab2b2f716f999
b-caby <caby.berenger@gmail.com>
2020-12-23 15:22:41 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angularDrag';
+  @ViewChild('person_1') person_1: ElementRef;
+  @ViewChild('person_2') person_2: ElementRef;
+  @ViewChild('person_3') person_3: ElementRef;
+
+  public displayedPerson: number = 1;
+  private initialAngle_1: number = 0;
+  private initialAngle_2: number = 20;
+  private initialAngle_3: number = 40;
+
+  constructor(private renderer: Renderer2) { }
+
+  onPanMove(evt)
+  {
+      this.renderer.setStyle(this.person_1.nativeElement, 'transform', `rotate(${ this.initialAngle_1 + evt.deltaX / 18 }deg)`);
+      this.renderer.setStyle(this.person_2.nativeElement, 'transform', `rotate(${ this.initialAngle_2 + evt.deltaX / 18 }deg)`);
+      this.renderer.setStyle(this.person_3.nativeElement, 'transform', `rotate(${ this.initialAngle_3 + evt.deltaX / 18 }deg)`);
+  }
+
+  onPanEnd(evt)
+  {
+    if (evt.deltaX < -160 && this.displayedPerson != 3)
+    {
+      this.initialAngle_1 -= 20;
+      this.initialAngle_2 -= 20;
+      this.initialAngle_3 -= 20;
+      this.displayedPerson++;
+    }
+
+    if (evt.deltaX > 160 && this.displayedPerson != 1)
+    {
+      this.initialAngle_1 += 20;
+      this.initialAngle_2 += 20;
+      this.initialAngle_3 += 20;
+      this.displayedPerson--;
+    }
+
+    this.renderer.setStyle(this.person_1.nativeElement, 'transform', `rotate(${ this.initialAngle_1 }deg)`);
+    this.renderer.setStyle(this.person_2.nativeElement, 'transform', `rotate(${ this.initialAngle_2 }deg)`);
+    this.renderer.setStyle(this.person_3.nativeElement, 'transform', `rotate(${ this.initialAngle_3 }deg)`);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import * as Hammer from 'hammerjs';
+import { HammerModule, HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
+
+export class MyHammerConfig extends HammerGestureConfig {
+  overrides = <any> {
+    swipe: { direction: Hammer.DIRECTION_ALL },
+  };
+}
+
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, HammerModule],
+  providers: [
+    { provide: HAMMER_GESTURE_CONFIG, useClass: MyHammerConfig },
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
