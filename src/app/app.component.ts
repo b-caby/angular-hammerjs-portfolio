8c51915d7589eb0823605d064b58a0bd28a89060
b-caby <caby.berenger@gmail.com>
2020-12-26 03:07:14 +0100

    feat: added flag buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent {
   @ViewChild('person_2') person_2: ElementRef;
   @ViewChild('person_3') person_3: ElementRef;
   @ViewChild('blob') blob: ElementRef;
-  @ViewChild('bubble') bubble: ElementRef;
+  @ViewChild('bubble') bubblePath: ElementRef;
   @ViewChild('word') word: ElementRef;
 
   public displayedPerson: number = 1;
@@ -25,43 +25,87 @@ export class AppComponent {
 
   constructor(private renderer: Renderer2) { }
 
-  // Initialization after view initialization to be able to render properly.
   ngAfterViewInit()
   {
+    // Initialization after view initialization to be able to render properly.
+    // Get the French word text from the JSON file.
     this.displayedText = translation.translations[0]["1"];
+    // Get the Frecn word position from the JSON file.
     let left = translation.translations[0]['1-left'];
     this.renderer.setStyle(this.word.nativeElement, 'left', `${left}px`); 
   }
 
   onPanMove(evt)
   {
-    // Move the persons.
+    // Do not allow anything to move if we are at the limits of the scroll.
     if((this.displayedPerson === 1 && evt.deltaX < 0) || (this.displayedPerson === 3 && evt.deltaX > 0) || (this.displayedPerson === 2))
     {
+      // This allows the persons to rotate with the panValue.
       this.move(this.person_1, this.initialAngle_1, evt);
       this.move(this.person_2, this.initialAngle_2, evt);
       this.move(this.person_3, this.initialAngle_3, evt);
 
-      // Move the bubble and hide the text.
+      // At 50% of the necessary panValue to switch, start moving the bubble.
+      // Do not hide the text if this treshold value is not reached.
+      // If the bubble Left is displayed, then the bubble Right need to start loading.
+      // The Left person will therefore be there.
       if ((this.displayedPerson === 1 && evt.deltaX < -80) || (this.displayedPerson === 3 && evt.deltaX > 80)) 
       {
         this.hideText();
-        this.isBubbleLeft = false;
+        this.moveBubble(this.bubbleRight, true);
       }
+      // If the bubble Right is displayed, then the bubble Left need to start loading.
+      // The Right person will therefore be there.
       else if ((this.displayedPerson === 2 && evt.deltaX < -80) || (this.displayedPerson === 2 && evt.deltaX > 80))
       {
         this.hideText();
-        this.isBubbleLeft = true;
-      }
-
-      let bubble = this.isBubbleLeft ? this.bubbleLeft : this.bubbleRight;
-      this.moveBubble(bubble, this.displayedPerson % 2 === 0);
+        this.moveBubble(this.bubbleLeft, false);
+      }    
     }
+  }
+
+  /**
+    Private function to rotate the persons with the value of the pan.
+  */
+  move(person: ElementRef, angle: number, event: any)
+  {
+    this.renderer.setStyle(person.nativeElement, 'transform', `rotate(${ angle + event.deltaX / 18 }deg)`);
+    this.renderer.removeStyle(person.nativeElement, 'transition-property');
+    this.renderer.removeStyle(person.nativeElement, 'transition-duration');
+  }
+
+  /**
+   * Private function to hide the text.
+   */
+  hideText()
+  {
+    // It is necessary to reset the transition because the show text id different. 
+    this.renderer.setStyle(this.word.nativeElement, 'opacity', '0');
+    this.renderer.setStyle(this.word.nativeElement, 'transition-property', 'opacity');
+    this.renderer.setStyle(this.word.nativeElement, 'transition-duration', '0s');
+    this.renderer.setStyle(this.word.nativeElement, 'transition-timing-function', 'linear');
+  }
+
+  /**
+    Private function to morph to the correct bubble and position it correctly.
+  */
+
+  moveBubble(bubblepath: string, isPersonLeftDisplayed: boolean)
+  {
+    // Morph the bubble.
+    this.renderer.setStyle(this.bubblePath.nativeElement, 'd', `path('${ bubblepath }')`);
+    this.renderer.setStyle(this.bubblePath.nativeElement, 'transition', '1.3s');
+
+    // Position the bubble correctly.
+    if (isPersonLeftDisplayed) this.renderer.setStyle(this.blob.nativeElement, 'left', '61px');
+    else this.renderer.setStyle(this.blob.nativeElement, 'left', '33px');
+    this.renderer.setStyle(this.blob.nativeElement, 'transition-property', 'left');
+    this.renderer.setStyle(this.blob.nativeElement, 'transition-duration', '1.3s');
   }
 
   onPanEnd(evt)
   {
-    // Scroll left.
+    // The scrolling finger is moving towards the left of the screen.
     // Double scroll only possible for person 1.
     if (evt.deltaX < -320 && this.displayedPerson === 1)
     {
@@ -75,7 +119,7 @@ export class AppComponent {
       this.displayedPerson++;
     }
 
-    // Scroll right.
+    // The scrolling finder is moving towards the right of the screen.
     // Double scroll only possible for person 3.
     if (evt.deltaX > 320 && this.displayedPerson === 3)
     {
@@ -89,28 +133,25 @@ export class AppComponent {
       this.displayedPerson--;
     }
 
-    // Move persons to defined positions.
+    // Snap persons to defined positions.
     this.stopMove(this.person_1, this.initialAngle_1);
     this.stopMove(this.person_2, this.initialAngle_2);
     this.stopMove(this.person_3, this.initialAngle_3);
 
     // Move bubble to defined position.
-    let isPersonLeftDisplayed = this.displayedPerson % 2 === 0
-    let bubble = isPersonLeftDisplayed ? this.bubbleRight : this.bubbleLeft;
-    this.isBubbleLeft = isPersonLeftDisplayed ? false : true; 
-    this.moveBubble(bubble, isPersonLeftDisplayed);
+    // After possible multiple scrolls, we need to determine which bubble is the correct one.
+    // If we display the bubble Left, then we display the person Right.
+    this.isBubbleLeft = this.displayedPerson % 2 === 0 ? false : true;
+    let bubble = this.displayedPerson % 2 === 0 ? this.bubbleRight : this.bubbleLeft;
+    this.moveBubble(bubble, !this.isBubbleLeft);
 
-    // Show text.
+    // Show text inside the bubble.
     this.showText(this.displayedPerson);
   }
 
-  move(person: ElementRef, angle: number, event: any)
-  {
-    this.renderer.setStyle(person.nativeElement, 'transform', `rotate(${ angle + event.deltaX / 18 }deg)`);
-    this.renderer.removeStyle(person.nativeElement, 'transition-property');
-    this.renderer.removeStyle(person.nativeElement, 'transition-duration');
-  }
-
+  /**
+   * Private function to snap the persons at the correct positions after the pan.
+   */
   stopMove(person: ElementRef, angle: number)
   {
     this.renderer.setStyle(person.nativeElement, 'transform', `rotate(${ angle }deg)`);
@@ -118,6 +159,98 @@ export class AppComponent {
     this.renderer.setStyle(person.nativeElement, 'transition-duration', '0.8s');
   }
 
+  /**
+   * Private function to display the correct text at the correct position.
+   */
+  showText(displayedPerson: number)
+  {
+    // This handles that the correct text is displayed.
+    this.displayedText = translation.translations[0][`${displayedPerson}`];
+
+    // This handles the Y-position (top) of the text.
+    if (displayedPerson % 2 === 0) this.renderer.setStyle(this.word.nativeElement, 'top', '174px');
+    else this.renderer.setStyle(this.word.nativeElement, 'top', '160px');
+
+    // This handles the X-position (left) of the text.
+    let left = translation.translations[0][`${displayedPerson}-left`];
+    this.renderer.setStyle(this.word.nativeElement, 'left', `${left}px`);
+
+    // Display the text with a bezier curve (slow at the beginning).
+    this.renderer.setStyle(this.word.nativeElement, 'opacity', '1');
+    this.renderer.setStyle(this.word.nativeElement, 'transition-property', 'opacity');
+    this.renderer.setStyle(this.word.nativeElement, 'transition-duration', '0.8s');
+    this.renderer.setStyle(this.word.nativeElement, 'transition-timing-function', 'cubic-bezier(1,0,1,0)');
+  }
+
+  onFrenchClicked()
+  {
+    // This is necessarry because one can click on the buttons when the anmations is not finished.
+    let currentDisplayed = this.displayedPerson;
+    this.displayedPerson = 1;
+    this.hideText();
+
+    // If we were on UK, turn 20deg. 
+    // If we were on NL, turn 40deg.
+    if (currentDisplayed === 2) this.addAngle(20);
+    else if (currentDisplayed === 3) this.addAngle(40);
+
+    // Snap the position of the persons.
+    this.stopMove(this.person_1, this.initialAngle_1);
+    this.stopMove(this.person_2, this.initialAngle_2);
+    this.stopMove(this.person_3, this.initialAngle_3);
+    // Moves the bubble.
+    this.moveBubble(this.bubbleLeft, false);
+    // Show text inside the bubble.
+    this.showText(this.displayedPerson);
+  }
+
+  onEnglishClicked()
+  {
+    // This is necessarry because one can click on the buttons when the anmations is not finished.
+    let currentDisplayed = this.displayedPerson;
+    this.displayedPerson = 2;
+    this.hideText();
+
+    // If we were on FR, turn 20deg. 
+    // If we were on NL, turn 20deg.
+    if (currentDisplayed === 3) this.addAngle(20);
+    else if (currentDisplayed === 1) this.substractAngle(20);
+
+    // Snap the position of the persons.
+    this.stopMove(this.person_1, this.initialAngle_1);
+    this.stopMove(this.person_2, this.initialAngle_2);
+    this.stopMove(this.person_3, this.initialAngle_3);
+    // Moves the bubble.
+    this.moveBubble(this.bubbleRight, true);
+    // Show text inside the bubble.
+    this.showText(this.displayedPerson);
+  }
+
+  onDutchClicked()
+  {
+    // This is necessarry because one can click on the buttons when the anmations is not finished.
+    let currentDisplayed = this.displayedPerson;
+    this.displayedPerson = 3;
+    this.hideText();
+
+    // If we were on UK, turn 20deg. 
+    // If we were on FR, turn 40deg.
+    if (currentDisplayed === 2) this.substractAngle(20);
+    else if (currentDisplayed === 1) this.substractAngle(40);
+
+    // Snap the position of the persons.
+    this.stopMove(this.person_1, this.initialAngle_1);
+    this.stopMove(this.person_2, this.initialAngle_2);
+    this.stopMove(this.person_3, this.initialAngle_3);
+    // Moves the bubble.
+    this.moveBubble(this.bubbleLeft, false);
+    // Show text inside the bubble.
+    this.showText(this.displayedPerson);
+  }
+
+  /**
+   * Private function to substract angles.
+   */
   substractAngle(angle: number)
   {
     this.initialAngle_1 -= angle;
@@ -125,6 +258,9 @@ export class AppComponent {
     this.initialAngle_3 -= angle;
   }
 
+  /**
+   * Private function to add angles.
+   */
   addAngle(angle: number)
   {
     this.initialAngle_1 += angle;
@@ -132,50 +268,4 @@ export class AppComponent {
     this.initialAngle_3 += angle;
   }
 
-  moveBubble(bubblepath: string, isPersonLeftDisplayed: boolean)
-  {
-    this.renderer.setStyle(this.bubble.nativeElement, 'd', `path('${ bubblepath }')`);
-    this.renderer.setStyle(this.bubble.nativeElement, 'transition', '2s');
-
-    if (isPersonLeftDisplayed)
-    {
-      this.renderer.setStyle(this.blob.nativeElement, 'left', '61px');
-    }
-    else
-    {
-      this.renderer.setStyle(this.blob.nativeElement, 'left', '33px');
-    }
-
-    this.renderer.setStyle(this.blob.nativeElement, 'transition-property', 'left');
-    this.renderer.setStyle(this.blob.nativeElement, 'transition-duration', '2s');
-  }
-
-  hideText()
-  {
-    this.renderer.setStyle(this.word.nativeElement, 'opacity', '0');
-    this.renderer.setStyle(this.word.nativeElement, 'transition-property', 'opacity');
-    this.renderer.setStyle(this.word.nativeElement, 'transition-duration', '0s');
-    this.renderer.setStyle(this.word.nativeElement, 'transition-timing-function', 'linear');
-  }
-
-  showText(displayedPerson: number)
-  {
-    if (displayedPerson % 2 === 0)
-    {
-      this.renderer.setStyle(this.word.nativeElement, 'top', '174px');
-    }
-    else
-    {
-      this.renderer.setStyle(this.word.nativeElement, 'top', '160px');
-    }
-
-    let left = translation.translations[0][`${displayedPerson}-left`];
-    this.renderer.setStyle(this.word.nativeElement, 'left', `${left}px`);
-    this.displayedText = translation.translations[0][`${displayedPerson}`];
-
-    this.renderer.setStyle(this.word.nativeElement, 'opacity', '1');
-    this.renderer.setStyle(this.word.nativeElement, 'transition-property', 'opacity');
-    this.renderer.setStyle(this.word.nativeElement, 'transition-duration', '1s');
-    this.renderer.setStyle(this.word.nativeElement, 'transition-timing-function', 'cubic-bezier(1,0,1,0)');
-  }
 }
