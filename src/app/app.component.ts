2fb231a6b7de08c6acf9a754b30c0ce06b552554
b-caby <caby.berenger@gmail.com>
2020-12-27 21:37:09 +0100

    fix: final style improvement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,21 +15,25 @@ export class AppComponent {
   @ViewChild('word') word: ElementRef;
 
   public displayedPerson: number = 1;
-  public displayedText: string;
+  public displayedText: string = '';
   private initialAngle_1: number = 0;
   private initialAngle_2: number = 20;
   private initialAngle_3: number = 40;
   private isBubbleLeft: boolean = true;
-  private bubbleRight: string = "m 92.911375,41.57637 c 0.169244,0.718222 0.686447,4.262671 0.686447,4.262671 0,0 1.455236,6.148618 -6.926546,9.381018 C 78.28956,58.452459 57.206347,60.0455 50.506303,56.7901 43.806367,53.5347 42.969226,46.053 42.969226,46.053 c 0,0 -2.95295,2.5915 -9.532323,-2.7878 6.888669,1.4689 9.309452,-1.8201 9.309452,-1.8201 0,0 -0.06817,-9.1872 6.67833,-12.3347 6.746499,-3.1474 26.462807,-2.4991 32.891042,0.4628 6.428236,2.962 7.944683,6.222282 7.944683,6.222282 0,0 2.436576,4.871017 2.650965,5.780888 z";
-  private bubbleLeft: string = "m 65.12929,40.738971 c -5.872488,5.03082 -8.987585,0.274199 -8.987585,0.274199 0,0 -1.04613,6.509129 -6.87819,10.479958 C 43.431455,55.463957 17.256116,55.562 12.595934,50.935871 7.9357524,46.309742 7.892,41.061909 7.892,41.061909 c 0,0 -0.4956477,-2.859713 -0.6,-4.394774 -0.1043523,-1.535061 -3.009e-4,-4.796081 -3.009e-4,-4.796081 0,0 0.1781227,-4.803838 6.2971959,-8.909054 6.119073,-4.105216 34.000757,-4.009075 39.173544,1.597061 5.172787,5.606136 3.948262,13.035072 3.948262,13.035072 0,0 2.330748,4.361814 8.418589,3.144838 z";
+  private bubbleRight: string = 'm 92.911375,41.57637 c 0.169244,0.718222 0.686447,4.262671 0.686447,4.262671 0,0 1.455236,6.148618 -6.926546,9.381018 C 78.28956,58.452459 57.206347,60.0455 50.506303,56.7901 43.806367,53.5347 42.969226,46.053 42.969226,46.053 c 0,0 -2.95295,2.5915 -9.532323,-2.7878 6.888669,1.4689 9.309452,-1.8201 9.309452,-1.8201 0,0 -0.06817,-9.1872 6.67833,-12.3347 6.746499,-3.1474 26.462807,-2.4991 32.891042,0.4628 6.428236,2.962 7.944683,6.222282 7.944683,6.222282 0,0 2.436576,4.871017 2.650965,5.780888 z';
+  private bubbleLeft: string = 'm 65.12929,40.738971 c -5.872488,5.03082 -8.987585,0.274199 -8.987585,0.274199 0,0 -1.04613,6.509129 -6.87819,10.479958 C 43.431455,55.463957 17.256116,55.562 12.595934,50.935871 7.9357524,46.309742 7.892,41.061909 7.892,41.061909 c 0,0 -0.4956477,-2.859713 -0.6,-4.394774 -0.1043523,-1.535061 -3.009e-4,-4.796081 -3.009e-4,-4.796081 0,0 0.1781227,-4.803838 6.2971959,-8.909054 6.119073,-4.105216 34.000757,-4.009075 39.173544,1.597061 5.172787,5.606136 3.948262,13.035072 3.948262,13.035072 0,0 2.330748,4.361814 8.418589,3.144838 z';
 
   constructor(private renderer: Renderer2) { }
+
+  ngOnInit()
+  {
+    // Get the French word text from the JSON file.
+    this.displayedText = translation.translations[0]['1'];
+  }
 
   ngAfterViewInit()
   {
     // Initialization after view initialization to be able to render properly.
-    // Get the French word text from the JSON file.
-    this.displayedText = translation.translations[0]["1"];
     // Get the Frecn word position from the JSON file.
     let left = translation.translations[0]['1-left'];
     this.renderer.setStyle(this.word.nativeElement, 'left', `${left}px`); 
@@ -168,7 +172,7 @@ export class AppComponent {
     this.displayedText = translation.translations[0][`${displayedPerson}`];
 
     // This handles the Y-position (top) of the text.
-    if (displayedPerson % 2 === 0) this.renderer.setStyle(this.word.nativeElement, 'top', '174px');
+    if (displayedPerson % 2 === 0) this.renderer.setStyle(this.word.nativeElement, 'top', '172px');
     else this.renderer.setStyle(this.word.nativeElement, 'top', '160px');
 
     // This handles the X-position (left) of the text.
